fd9af1e4f1d9a01fe8b8d929e15d480cd1175561
bastiano86 <sebastiano_lamonaca@yahoo.com>
2021-08-27 16:33:40 +0200

    scrivo dei commenti accurati al javascript per comprendere bene i passaggi

---
+++ script.js
@@ -0,0 +1,31 @@
+
+/**
+ * A -
+ * Generatore di "nomi cognomi" casuali: prendendo una lista di nomi e 
+ * una lista di cognomi, Gatsby vuole generare una falsa lista di 8 invitati.
+ *
+ * B - Stampiamo la lista in modo ordinato in html come elementi <li> figli di un unico <ul>.
+ */
+
+//CARICHIAMO LE DUE LISTE
+listaNomi = ["Adam", "Alex", "Aaron", "Ben", "Carly", "Dan", "David", "Edward", "Fred", "Frank", "George", "Hal", "Hank", "Ike", "John", "Jack", "Joe", "Larry", "Monte", "Matthew", "Mark", "Nathan", "Otto", "Paul", "Peter", "Roger", "Roger", "Steve", "Thomas", "Tim", "Ty", "Victor", "Walter"];
+listaCognomi = ["Anderson", "Ashwoon", "Aikin", "Bateman", "Bongard", "Bowers", "Boyd", "Cannon", "Cast", "Deitz", "Dewalt", "Ebner", "Frick", "Hancock", "Haworth", "Hesch", "Hoffman", "Kassing", "Knutson", "Lawless", "Lawicki", "Mccord", "McCormack", "Miller", "Myers", "Nugent", "Ortiz", "Orwig", "Ory", "Paiser", "Pak", "Pettigrew", "Quinn", "Quizoz", "Ramachandran", "Resnick", "Sagar", "Schickowski", "Schiebel", "Sellon", "Severson", "Shaffer", "Solberg", "Soloman", "Sonderling", "Soukup", "Soulis", "Stahl", "Sweeney", "Tandy", "Trebil", "Trusela", "Trussel", "Turco", "Uddin", "Uflan", "Ulrich", "Upson", "Vader", "Vail", "Valente", "Van Zandt", "Vanderpoel", "Ventotla", "Vogal", "Wagle", "Wagner", "Wakefield", "Weinstein", "Weiss", "Woo", "Yang", "Yates", "Yocum", "Zeaser", "Zeller", "Ziegler", "Bauer", "Baxster", "Casal", "Cataldi", "Caswell", "Celedon", "Chambers", "Chapman", "Christensen", "Darnell", "Davidson", "Davis", "DeLorenzo", "Dinkins", "Doran", "Dugelman", "Dugan", "Duffman", "Eastman", "Ferro", "Ferry", "Fletcher", "Fietzer", "Hylan", "Hydinger", "Illingsworth", "Ingram", "Irwin", "Jagtap", "Jenson", "Johnson", "Johnsen", "Jones", "Jurgenson", "Kalleg", "Kaskel", "Keller", "Leisinger", "LePage", "Lewis", "Linde", "Lulloff", "Maki", "Martin", "McGinnis", "Mills", "Moody", "Moore", "Napier", "Nelson", "Norquist", "Nuttle", "Olson", "Ostrander", "Reamer", "Reardon", "Reyes", "Rice", "Ripka", "Roberts", "Rogers", "Root", "Sandstrom", "Sawyer", "Schlicht", "Schmitt", "Schwager", "Schutz", "Schuster", "Tapia", "Thompson", "Tiernan", "Tisler"];
+ 
+//PREDISPONIAMO UNA LISTA VUOTA
+listaInvitati = [];
+
+let contatore = 0
+
+//FINCHé DURERà LA LISTA DEI NOMI
+while (contatore < listaNomi.length) {
+    //SE DI listaNomi LA LETTERA FINALE SARà UGUALE AD 'a'   
+    if (listaNomi[contatore].charAt(listaNomi[contatore].length - 1) == 'a ||'
+    // OPPURE 'e'
+        listaNomi[contatore].charAt(listaNomi[j].length - 1) == 'e' || 
+    // OPPURE 'i'
+        listaNomi[j].charAt(listaNomi[j].length - 1) == 'io' ||
+}
+
+
+
+
